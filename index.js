4e6f83335090fc4c2008e007dbacf49e6511f551
Denise Pendleton <dpendl1@hotmail.com>
2018-06-03 22:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return  nested
 }
 
 function increaseRankBy(n){
-  ranks = querySelectorAll(ul.ranked-list).querySelectorAll(li)
+  let ranks = querySelectorAll(ul.ranked-list).querySelectorAll(li)
   return ranks.map(rank => rank.innerHTML + n)
 
 }
