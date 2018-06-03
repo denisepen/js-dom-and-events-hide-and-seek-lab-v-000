c316ab073250407d22bbc15c379c4a0f78fa765c
Denise Pendleton <dpendl1@hotmail.com>
2018-06-03 22:15:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return  nested
 }
 
 function increaseRankBy(n){
-  ranks = querySelectorAll(ul.ranked-list li).innerHTML;
+  ranks = querySelectorAll(ul.ranked-list li);
   return ranks.map(rank => rank + n)
 }
 
