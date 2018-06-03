8dec761e5ff6d3c8abd052bd2cc1b36581b3af93
Denise Pendleton <dpendl1@hotmail.com>
2018-06-03 21:49:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ return  document.querySelector(selector)
 }
 
 function nestedTarget(){
-
+  nested = document.getElementById("nested")
+return  nested.querySelector("target")
 }
 
 function increaseRankBy(n){
