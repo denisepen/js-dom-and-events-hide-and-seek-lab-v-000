b68764976aece0683ad773ed543219a1550b0530
Denise Pendleton <dpendl1@hotmail.com>
2018-06-03 23:05:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ return  nested
 
 function increaseRankBy(n){
   let ranks = document.getElementById("app").querySelectorAll("ul.ranked-list li")
-   return ranks.map(rank => parseInt(rank.innerHTML) + n )
-  // for (let i = 0; i < ranks.length; i++){
-  //   ranks[i].innerHTML =    parseInt(ranks[i].innerHTML) + n
-  // }
+  //  return ranks.map(rank => parseInt(rank.innerHTML) + n )
+  for (let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML =    parseInt(ranks[i].innerHTML) + n
+  }
 
 }
 
