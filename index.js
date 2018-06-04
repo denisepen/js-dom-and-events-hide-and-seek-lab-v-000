976e7825a4b5b987654169732791fb5553d4a701
Denise Pendleton <dpendl1@hotmail.com>
2018-06-04 00:37:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function increaseRankBy(n){
 
 function deepestChild(){
 let node = document.getElementById("grand-node").querySelector("div").querySelector("div").querySelector("div").querySelector("div")
-  return node.innerHTML
+  return node
 }
