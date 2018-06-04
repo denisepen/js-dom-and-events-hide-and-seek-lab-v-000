90783c572a3eb43e078151de7bb18a89b2fce616
Denise Pendleton <dpendl1@hotmail.com>
2018-06-04 00:37:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+return  document.querySelector(selector)
+}
+
+function nestedTarget(){
+  nested = document.getElementById("nested").querySelector("div.target")
+return  nested
+}
+
+function increaseRankBy(n){
+  let ranks = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  //  return ranks.map(rank => parseInt(rank.innerHTML) + n )
+  for (let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML =    parseInt(ranks[i].innerHTML) + n
+  }
+
+}
+
+function deepestChild(){
+let node = document.getElementById("grand-node").querySelector("div").querySelector("div").querySelector("div").querySelector("div")
+  return node
+}
